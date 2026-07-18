c91cbd1ab81e02d149505b300cff0aff637461cd
RajaRajeshwari11 <budeviyadav123@gmail.com>
2026-07-18 19:41:50 +0530

    helloworld
    hello

---
+++ app.java
@@ -0,0 +1,8 @@
+// A simple Java program to print "Hello, World!" to the console
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Print the message to the console
+        System.out.println("Hello, World!");
+    }
+}
